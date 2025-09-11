1b79f342e10e949be749eccb575ceb9a25b64ff1
Henrik Schack <hschack@gmail.com>
2025-09-11 23:12:58 +0200

    med overvågning

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,180 @@
 #include <Arduino.h>
+#include <EEPROM.h>
 
-// constants won't change. Used here to set a pin number:
-const int ledPin = LED_BUILTIN;  // the number of the LED pin
-const int potPin = A0;  // Analog input A0
-const int currentPin = A1;
-const int carBat = A2;
-const int liefpoBat = A3;
-// Variables will change:
-int ledState = LOW;  // ledState used to set the LED
-const int pwmPin = 9;  // PWM pin 9 på Uno
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;  // will store last time LED was updated
+// ------------------- Pin constants -------------------
+const int ledPin       = LED_BUILTIN;
+const int currentPin   = A1; // ACS758
+const int carBatPin    = A2;
+const int liefpoBatPin = A3;
+const int pwmPin       = 5;
 
-// constants won't change:
-const long interval = 1000;  // interval at which to blink (milliseconds)
+// ------------------- ADC / sensor constants -------------------
+constexpr float Vref = 5.0;
+constexpr float scaleDirect = Vref / 1023.0;
 
+// Voltage dividers for batteries (carBat & LiFePO4)
+constexpr float R1 = 8200.0;
+constexpr float R2 = 2200.0;
+constexpr float scaleDivider = (Vref / 1023.0) * ((R1 + R2) / R2);
 
+// ACS758 parameters
+constexpr float acsOffset = 2.50; // 0A
+constexpr float acsSens   = 0.040; // 40 mV per 1A
 
+// ------------------- Safety thresholds -------------------
+constexpr float BAT_DIFF_MAX   = 0.2;   // V, carBat - LiFePO4
+constexpr float LIFEPo_MAX     = 14.0;  // V
+constexpr float LIFEPo_RECOVER = 13.5;  // V, resume charging
+constexpr float ACS_MIN        = -1.0;  // A, stop if current goes negative
 
+// ------------------- PWM control constants -------------------
+constexpr float SETPOINT_A     = 20.0;  // target current
+constexpr float PWM_STEP_FAST  = 0.05;  // 5% PWM step
+constexpr float PWM_STEP_SLOW  = 0.02;  // 2% PWM step
+constexpr int   PWM_MAX        = 255;
+constexpr int   PWM_MIN        = 0;
+
+// ------------------- Timing -------------------
+unsigned long lastAdcTime   = 0;
+unsigned long lastPrintTime = 0;
+constexpr unsigned long adcInterval   = 100;   // 10 Hz
+constexpr unsigned long printInterval = 1000;  // 1 Hz
+
+// ------------------- Filtered ADC -------------------
+uint16_t filtCurrent   = 0;
+uint16_t filtCarBat    = 0;
+uint16_t filtLiFePO4   = 0;
+
+// ------------------- PWM state -------------------
+int pwmOut = 0;
+
+// ------------------- Charge permission flag -------------------
+bool doCharge = true;   // true = charging allowed
+
+// EEPROM address to store flag
+constexpr int EEPROM_ADDR_FLAG = 0;
+
+// ------------------- Function prototypes -------------------
+void sampleAdc();
+uint16_t filteredUpdate(uint16_t oldVal, int newVal);
+void controlPWM(float measuredAmp);
+void printStatus(float measuredAmp);
+void batterySafetyCheck(float carVolt, float lifepoVolt, float measuredAmp);
+bool readEepromFlag();
+void writeEepromFlag(bool flag);
+
+// ------------------- Setup -------------------
 void setup() {
-  // Sæt PWM-pinen til output
-  pinMode(pwmPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
-  // Du kan starte Serial Monitor for at se værdierne (valgfrit)
-  Serial.begin(115200);
+    pinMode(ledPin, OUTPUT);
+    pinMode(pwmPin, OUTPUT);
+    analogWrite(pwmPin, pwmOut);
+    Serial.begin(115200);
+
+    // Read flag from EEPROM (0xFF = allow charging)
+    doCharge = readEepromFlag();
 }
 
+// ------------------- Main loop -------------------
 void loop() {
-  unsigned long currentMillis = millis();
-  // Læs værdien fra potentiometeret (0 - 1023)
-  int sensorValue = analogRead(potPin);
-  
-  // Konverter den analoge læsning (0-1023) til en PWM værdi (0-255)
-  // Ved at mappe (map) værdien
-  int pwmValue = map(sensorValue, 0, 1023, 0, 255);
-  
-  // Skriv PWM-værdien til pinen
-  analogWrite(pwmPin, pwmValue);
+    unsigned long now = millis();
 
-  int powerValue = analogRead(currentPin);
-  float power = (powerValue / 1023.0) * 5.0;
+    // ADC sampling 10 Hz
+    if (now - lastAdcTime >= adcInterval) {
+        lastAdcTime = now;
+        sampleAdc();
+    }
 
-  int carValue = analogRead(carBat);
-  float carValueV = (carValue / 1023.0) * 5.0;
+    // Control + safety check + print 1 Hz
+    if (now - lastPrintTime >= printInterval) {
+        lastPrintTime = now;
 
-  int liefpoValue = analogRead(liefpoBat);
-  float liefpoValueV = (liefpoValue / 1023.0) * 5.0;
+        // Convert ADCs to voltage / current
+        float carVolt     = filtCarBat * scaleDivider;
+        float lifepoVolt  = filtLiFePO4 * scaleDivider;
+        float currentVolt = filtCurrent * scaleDirect;
+        float measuredAmp = (currentVolt - acsOffset) / acsSens;
 
-  // (Valgfrit) Print værdierne til Serial Monitor
-  Serial.print("Pot: ");
-  Serial.print(sensorValue);
-  Serial.print(" | PWM: ");
-  Serial.print(pwmValue);
-  Serial.print(" | A: ");
-  Serial.print(power);
-  Serial.print(" | Car: ");
-  Serial.print(carValueV);
-  Serial.print(" | Liefpo: ");
-  Serial.println(liefpoValueV);
+        // Safety check and update doCharge
+        batterySafetyCheck(carVolt, lifepoVolt, measuredAmp);
 
-    if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
+        // Only control PWM if allowed
+        if (doCharge) controlPWM(measuredAmp);
 
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
+        // Print status
+        printStatus(measuredAmp);
+        digitalWrite(ledPin, !digitalRead(ledPin));
+    }
+}
+
+// ------------------- Functions -------------------
+
+// Sample all ADC channels and apply simple filter
+void sampleAdc() {
+    filtCurrent   = filteredUpdate(filtCurrent,   analogRead(currentPin));
+    filtCarBat    = filteredUpdate(filtCarBat,    analogRead(carBatPin));
+    filtLiFePO4   = filteredUpdate(filtLiFePO4,   analogRead(liefpoBatPin));
+}
+
+uint16_t filteredUpdate(uint16_t oldVal, int newVal) {
+    return (uint16_t)(oldVal * 0.9 + newVal * 0.1);
+}
+
+// Simple incremental PWM control
+void controlPWM(float measuredAmp) {
+    int step = 0;
+    if (measuredAmp < SETPOINT_A * 0.8) {
+        step = (int)(PWM_MAX * PWM_STEP_FAST); // +5%
+    } else if (measuredAmp < SETPOINT_A) {
+        step = (int)(PWM_MAX * PWM_STEP_SLOW); // +2%
     } else {
-      ledState = LOW;
+        step = -(int)(PWM_MAX * PWM_STEP_SLOW); // -2%
     }
+
+    pwmOut += step;
+    if (pwmOut > PWM_MAX) pwmOut = PWM_MAX;
+    if (pwmOut < PWM_MIN) pwmOut = PWM_MIN;
+
+    analogWrite(pwmPin, pwmOut);
+}
+
+// Print current & PWM
+void printStatus(float measuredAmp) {
+    Serial.print("Current: ");
+    Serial.print(measuredAmp, 2);
+    Serial.print(" A   PWM: 0x");
+    Serial.print(pwmOut, HEX);
+    Serial.print(" (");
+    Serial.print(pwmOut);
+    Serial.println(")");
+    Serial.print("Charge allowed: ");
+    Serial.println(doCharge ? "YES" : "NO");
+}
+
+// ------------------- Battery safety check -------------------
+void batterySafetyCheck(float carVolt, float lifepoVolt, float measuredAmp) {
+    bool prevFlag = doCharge;
+
+    if ((carVolt - lifepoVolt) < BAT_DIFF_MAX) {
+        doCharge = false;  // stop charging
+    } else if (lifepoVolt > LIFEPo_MAX) {
+        doCharge = false;  // stop charging
+    } else if (!doCharge && lifepoVolt < LIFEPo_RECOVER) {
+        doCharge = true;   // resume charging
     }
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
-  
-  // En lille forsinkelse for at gøre Serial output læsbart
-  // Dette kan gøres meget kortere eller fjernes helt efter behov
-  delay(500); 
+
+    // Stop if current goes negative beyond threshold
+    if (measuredAmp < ACS_MIN) doCharge = false;
+
+    // Write flag to EEPROM if changed
+    if (doCharge != prevFlag) writeEepromFlag(doCharge);
+}
+
+// ------------------- EEPROM helpers -------------------
+bool readEepromFlag() {
+    byte val = EEPROM.read(EEPROM_ADDR_FLAG);
+    return (val == 0xFF);
+}
+
+void writeEepromFlag(bool flag) {
+    EEPROM.update(EEPROM_ADDR_FLAG, flag ? 0xFF : 0x00);
 }
